7af4b788aa0b7195e3edb7681e9065381c23be4c
kevaljivani <jivanikeval978@gmail.com>
2021-12-04 15:30:18 +0530

    complete project

---
+++ node-express-jwt-auth/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const authController = require('../controllers/authController');
+const {requireAuth} = require('../middleware/authMiddleware')
+
+
+const router = Router();
+
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
+router.get('/logout', authController.logout_get);
+
+
+router.get('/about',requireAuth, authController.about_get);
+
+
+
+
+module.exports = router;
